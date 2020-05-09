b17088a6fd9d20525c797d227dc1f1e12e25f2a6
raphaelschwegler <raphael.schwegler@uni-weimar.de>
2020-05-09 21:42:31 +0200

    TDD Test Frame Running, Require replace with Check

--- source/tests.cpp
+++ source/tests.cpp
@@ -1,5 +1,18 @@
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
+# include < cmath >
+
+int gcd(int a, int b)
+{
+    return 1;
+}
+
+TEST_CASE(" describe_gcd ", "[gcd]")
+{
+    CHECK(gcd(2, 4) == 2);
+    CHECK(gcd(9, 6) == 3);
+    CHECK(gcd(3, 7) == 1);
+}
 
 int main(int argc, char* argv[])
 {
